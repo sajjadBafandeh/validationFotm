0b5606a73ec14b80f79d990e6c4f6ce24fd0c362
sajjad bafandeh <sajjad.bafandeh.9799@gmail.com>
2023-12-10 13:05:07 +0330

    update index

--- index.php
+++ index.php
@@ -1,10 +1,1 @@
 <?php
-require "vendor/autoload.php";
-use App\Controller\Validation;
-
-$data = [
-  'name'=>"sajjad"
-];
-
-$checking = new Validation();
-var_dump($checking->check($data , 'name'));
