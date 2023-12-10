55f84b5e9869dc064e64d0730a6fd5154a940b79
sajjad bafandeh <sajjad.bafandeh.9799@gmail.com>
2023-12-10 12:34:54 +0330

    update composer.json ad index.php

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
 <?php
+require "vendor/autoload.php";
+use App\Controller\Validation;
+
+$data = [
+  'name'=>"sajjad"
+];
+
+$checking = new Validation();
+var_dump($checking->check($data , 'name'));
